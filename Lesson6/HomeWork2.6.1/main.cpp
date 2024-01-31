097ab6106006ddf73a2724c85c09be9ec9225526
Igor Koshkin <kotoffski82@yandex.ru>
2024-01-31 19:39:53 +0300

    add changes

--- Lesson6/HomeWork2.6.1/main.cpp
+++ Lesson6/HomeWork2.6.1/main.cpp
@@ -1,16 +1,17 @@
 #include<iostream>
 
-#define MODE 5
-
-int add(int a, int b)
-{
-	return a + b;
-}
-int main()
-{
+#define MODE 1
 #ifndef MODE
 #error "You should difene MODE"
 #endif // !MODE
+
+#if MODE == 1
+#define add(a , b) ((a) + (b))
+#endif // MODE == 1
+
+
+int main()
+{
 
 #if MODE == 0
 
