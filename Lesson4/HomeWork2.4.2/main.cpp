7c28a369d1e09fe58f61a581340f37b30b339bb0
Igor Koshkin <kotoffski82@yandex.ru>
2024-02-04 18:56:55 +0300

    add changes

--- Lesson4/HomeWork2.4.2/main.cpp
+++ Lesson4/HomeWork2.4.2/main.cpp
@@ -1,37 +1,39 @@
 #include<iostream>
-// Родительский класс  треугольник 
+
 
 
 class Figure
 {
-	
-		// защищенные поля 
 protected:
 		std::string name;
-		int side_a = 10;
-		int side_b = 20;
-		int side_c = 30;
-		int side_d = 40;
-		int angel_a = 50;
-		int angel_b = 60;
-		int angel_c = 70;
-		int angel_d = 80;
+	
 public:
 
 	virtual void print_info()
 	{
-		std::cout << name << ":" << std::endl;
-		std::cout << "Sides : " << "a = " << side_a << " b = " << side_b << " c = " << side_c << " d = " << side_d << std::endl;
-		std::cout << "Angels : " << "A = " << angel_a << " B = " << angel_b << " C = " << angel_c << " D = " << angel_d << std::endl;
-		std::cout << "\n";
+		
 	}
 	
 };
 
+void print(Figure* figure)
+{
+	figure->print_info();
+}
+
 class Triangle : public Figure
 {
+
+protected:
+	int side_a = 10;
+	int side_b = 20;
+	int side_c = 30;
+	int angel_a = 50;
+	int angel_b = 60;
+	int angel_c = 70;
 	
 public:
+	Triangle() {}
 
 	Triangle(const int& side_a, const int& side_b, const int& side_c, const int& angel_a, const int& angel_b, const int& angel_c)
 	{
@@ -43,8 +45,6 @@ public:
 		this->angel_b = angel_b;
 		this->angel_c = angel_c;
 	}
-
-	//вывод информации  о треугольнике 
 	void print_info() override
 	{
 		std::cout << name << ":" << std::endl;
@@ -52,22 +52,132 @@ public:
 		std::cout << "Angels : " << "A = " << angel_a << " B = " << angel_b << " C = " << angel_c << std::endl;
 		std::cout << "\n";
 	}
-
-
 };
-//Функция вызова метода печати 
-void print(Figure* figure)
+
+class RightTriangle : public Triangle
 {
-	figure->print_info();
-}
+public:
+	RightTriangle(const int& side_a, const int& side_b, const int& side_c, const int& angel_a, const int& angel_b)
+	{
+		name = "Right triangle";
+		this->side_a = side_a;
+		this->side_b = side_b;
+		this->side_c = side_c;
+		this->angel_a = angel_a;
+		this->angel_b = angel_b;
+		this->angel_c = 90;
+	}
+		
+	void print_info() override
+	{
+		std::cout << name << ":" << std::endl;
+		std::cout << "Sides : " << "a = " << side_a << " b = " << side_b << " c = " << side_c << std::endl;
+		std::cout << "Angels : " << "A = " << angel_a << " B = " << angel_b << " C = " << angel_c << std::endl;
+		std::cout << "\n";
+	}
+};
 
-
-  
-class Rectangle :public Figure
+class IsoscelesTriangle : public Triangle
 {
+public:
+	IsoscelesTriangle(const int& side_a, const int& side_b, const int& angel_a, const int& angel_b)
+	{
+		
+		name = "IsoscelesTriangle";
+		this->side_a = side_a;
+		this->side_b = side_b;
+		this->angel_a = angel_a;
+		this->angel_b = angel_b;
+		side_c = side_a;
+		angel_c = angel_a;
+	}
+	void print_info() override
+	{
+		std::cout << name << ":" << std::endl;
+		std::cout << "Sides : " << "a = " << side_a << " b = " << side_b << " c = " << side_c << std::endl;
+		std::cout << "Angels : " << "A = " << angel_a << " B = " << angel_b << " C = " << angel_c << std::endl;
+		std::cout << "\n";
+	}
+};
 
+class EquilateralTriangle : public Triangle
+{
+public:
+	EquilateralTriangle(const int& side_a)
+	{
+
+		name = "IsoscelesTriangle";
+		this->side_a = side_a;
+		this->angel_a = 60;
+		this->angel_b = 60;
+		this->angel_c = 60; 
+		side_c = side_a;
+		side_b = side_a;
+		
+	}
+	void print_info() override
+	{
+		std::cout << name << ":" << std::endl;
+		std::cout << "Sides : " << "a = " << side_a << " b = " << side_b << " c = " << side_c << std::endl;
+		std::cout << "Angels : " << "A = " << angel_a << " B = " << angel_b << " C = " << angel_c << std::endl;
+		std::cout << "\n";
+	}
+};
+
+class Quadrangle :public Figure
+{
+protected:
+	int side_a = 10;
+	int side_b = 20;
+	int side_c = 30;
+	int side_d = 40;
+	int angel_a = 50;
+	int angel_b = 60;
+	int angel_c = 70;
+	int angel_d = 80;
 
 public:
+
+	Quadrangle(){}
+
+	Quadrangle(const int& side_a, const int& side_b, const int& side_c,const int& side_d, const int& angel_a, const int& angel_b, const int& angel_c,const int& angel_d)
+	{
+		name = "Quadrangle";
+		this->side_a = side_a;
+		this->side_b = side_b;
+		this->side_c = side_c;
+		this->side_d = side_d;
+
+		this->angel_a = angel_a;
+		this->angel_b = angel_b;
+		this->angel_c = angel_c;
+		this->angel_d = angel_d;
+
+	}
+	void print_info() override
+	{
+		std::cout << name << ":" << std::endl;
+		std::cout << "Sides : " << "a = " << side_a << " b = " << side_b << " c = " << side_c << " d = " << side_d << std::endl;
+		std::cout << "Angels : " << "A = " << angel_a << " B = " << angel_b << " C = " << angel_c << " D = " << angel_d << std::endl;
+		std::cout << "\n";
+	}
+};
+
+class Rectangle :public Figure
+{
+protected:
+	int side_a = 10;
+	int side_b = 20;
+	int side_c = 30;
+	int side_d = 40;
+	int angel_a = 50;
+	int angel_b = 60;
+	int angel_c = 70;
+	int angel_d = 80;
+
+public:
+
+	Rectangle(){}
 
 	Rectangle(const int& side_a, const int& side_b)
 	{
@@ -90,89 +200,26 @@ public:
 		std::cout << "Angels : " << "A = " << angel_a << " B = " << angel_b << " C = " << angel_c << " D = " << angel_d << std::endl;
 		std::cout << "\n";
 	}
-
-
 };
 
-
-/*
-
-class RightTriangle : public Triangle
-{
-public:
-	RightTriangle()
-	{
-		angel_c = 90;
-		name = "Right triangle";
-	}
-	void print_info() override
-	{
-		std::cout << name << ":" << std::endl;
-		std::cout << "Sides : " << "a = " << side_a << " b = " << side_b << " c = " << side_c << std::endl;
-		std::cout << "Angels : " << "A = " << angel_a << " B = " << angel_b << " C = " << angel_c << std::endl;
-		std::cout << "\n";
-	}
-};
-
-class IsoscelesTriangle : public Triangle
-{
-public:
-	IsoscelesTriangle()
-	{
-		side_c = side_a;
-		angel_c = angel_a;
-		name = "IsoscelesTriangle";
-	}
-	void print_info() override
-	{
-		std::cout << name << ":" << std::endl;
-		std::cout << "Sides : " << "a = " << side_a << " b = " << side_b << " c = " << side_c << std::endl;
-		std::cout << "Angels : " << "A = " << angel_a << " B = " << angel_b << " C = " << angel_c << std::endl;
-		std::cout << "\n";
-	}
-};
-
-
-
-//Классы наследники  для  четырехугольника 
-
-class Rectangle : public Quadrangle
-{
-public:
-	Rectangle()
-	{
-		angel_a = 90;
-		angel_b = 90;
-		angel_c = 90;
-		angel_d = 90;
-
-		side_c = side_a;
-		side_d = side_b;
-
-		name = "Rectangle";
-	}
-	void print_info() override
-	{
-		std::cout << name << ":" << std::endl;
-		std::cout << "Sides : " << "a = " << side_a << " b = " << side_b << " c = " << side_c << " d = " << side_d << std::endl;
-		std::cout << "Angels : " << "A = " << angel_a << " B = " << angel_b << " C = " << angel_c << " D = " << angel_d << std::endl;
-		std::cout << "\n";
-	}
-};
-
-// Для квадрата  базовый класс прямоугольник 
 class Square : public Rectangle
 {
 public:
-	Square()
+
+	Square() {}
+
+	Square(const int& side_a)
 	{
-
-		side_a = 50;
-		side_b = 50;
-		side_c = 50;
-		side_d = 50;
-
 		name = "Square";
+		this->side_a = side_a;
+		side_b = side_a;
+		side_c = side_a;
+		side_d = side_a;
+
+		this->angel_a = 90;
+		this->angel_b = 90;
+		this->angel_c = 90;
+		this->angel_d = 90;;
 	}
 	void print_info() override
 	{
@@ -183,19 +230,21 @@ public:
 	}
 };
 
-class Parallelogram : public Quadrangle
+class Parallelogram : public Rectangle
 {
 public:
-	Parallelogram()
+	Parallelogram(const int& side_a, const int& side_b, const int& angel_a, const int& angel_b)
 	{
-
-		angel_c = angel_a;
-		angel_d = angel_b;
-
+		name = "Parallelogram";
+		this->side_a = side_a;
+		this->side_b = side_b;
 		side_c = side_a;
 		side_d = side_b;
 
-		name = "Parallelogram";
+		this->angel_a = angel_a;
+		this->angel_b = angel_b;
+		angel_c = angel_a;
+		angel_d = angel_b;
 	}
 	void print_info() override
 	{
@@ -206,18 +255,23 @@ public:
 	}
 };
 
-//Для ромба возьмем базовый класс квадрат  
 class Rhombus : public Square
 {
 public:
-	Rhombus()
+	Rhombus(const int& side_a, const int& angel_a, const int& angel_b)
 	{
-		angel_a = 30;
+		name = "Rhombus";
+		this->side_a = side_a;
+		side_c = side_a;
+		side_b = side_a;
+		side_d = side_a;
+
+		this->angel_a = angel_a;
+		this->angel_b = angel_b;
 		angel_c = angel_a;
-		angel_b = 50;
 		angel_d = angel_b;
 
-		name = "Rhombus";
+		
 	}
 	void print_info() override
 	{
@@ -227,52 +281,30 @@ public:
 		std::cout << "\n";
 	}
 };
-*/
+
 int main()
 {
 	Figure figure;
 	Triangle triangle(20, 30, 800, 500, 600, 700);
+	RightTriangle right_triangle(5, 10, 15, 20, 30);
+	IsoscelesTriangle isosceles_triangle(25,30,100,70);
+	EquilateralTriangle equilateral_triangle(125);
 	Rectangle rectangle(50, 60);
-	
-	
+	Quadrangle quadrangle(41,42,43,44,45,46,47,48);
+	Square square(555);
+	Parallelogram parallelogram(51, 52, 53, 54);
+	Rhombus rhombus(75, 123, 124);
+
 	print(&triangle);
+	print(&right_triangle);
+	print(&isosceles_triangle);
+	print(&equilateral_triangle);
+	print(&quadrangle);
 	print(&rectangle);
-	/*
-	RightTriangle right_triangle;
-	IsoscelesTriangle isosceles_triangle;
-
-	Quadrangle qudrangle;
-	Rectangle rectangle;
-	Square square;
-	Parallelogram parallelogram;
-	Rhombus rhombus;
+	print(&square);
+	print(&parallelogram);
+	print(&rhombus);
 	
-
-	Triangle* ptr_triangle = &triangle;
-	ptr_triangle->print_info();
-
-	Triangle* ptr_right_triangle = &right_triangle;
-	ptr_right_triangle->print_info();
-
-	Triangle* ptr_isosceles_triangle = &isosceles_triangle;
-	ptr_isosceles_triangle->print_info();
-
-	Quadrangle* ptr_qudrangle = &qudrangle;
-	ptr_qudrangle->print_info();
 	
-	Quadrangle* ptr_rectangle = &rectangle;
-	ptr_rectangle->print_info();
-
-	Quadrangle* ptr_square = &square;
-	ptr_square->print_info();
-
-	Quadrangle* ptr_parallelogram = &parallelogram;
-	ptr_parallelogram->print_info();
-
-	Quadrangle* ptr_rhombus = &rhombus;
-	ptr_rhombus->print_info();
-	*/
-
-
 	return 0;
 }
---
+++ Lesson8/HomeWork2.8.1/main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+class Fraction
+{
+private:
+	int numerator_;
+	int denominator_;
+
+public:
+	Fraction(int numerator, int denominator)
+	{
+		numerator_ = numerator;
+		denominator_ = denominator;
+	}
+	
+	bool operator == (const Fraction& other)
+	{
+		return ((this->numerator_ * other.denominator_) == (this->denominator_ * other.numerator_));
+	}
+	bool operator != (const Fraction& other)
+	{
+		return ((this->numerator_ * other.denominator_) != (this->denominator_ * other.numerator_));
+	}
+	bool operator < (const Fraction& other)
+	{
+		return ((this->numerator_ * other.denominator_) < (this->denominator_ * other.numerator_));
+	}
+	bool operator > (const Fraction& other)
+	{
+		return ((this->numerator_ * other.denominator_) > (this->denominator_ * other.numerator_));
+	}
+	bool operator <= (const Fraction& other)
+	{
+		return ((this->numerator_ * other.denominator_) <= (this->denominator_ * other.numerator_));
+	}
+	bool operator >= (const Fraction& other)
+	{
+		return ((this->numerator_ * other.denominator_) >= (this->denominator_ * other.numerator_));
+	}
+
+};
+
+int main()
+{
+	Fraction f1(4, 3);
+	Fraction f2(6, 11);
+
+	std::cout << "f1" << ((f1 == f2) ? " == " : " not == ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 != f2) ? " != " : " not != ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 < f2) ? " < " : " not < ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 > f2) ? " > " : " not > ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 <= f2) ? " <= " : " not <= ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 >= f2) ? " >= " : " not >= ") << "f2" << '\n';
+	return 0;
+}
