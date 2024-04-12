7608ef971b212a5b82f4406aa625ab3e3a21f74b
Igor Koshkin <kotoffski82@yandex.ru>
2024-04-12 09:57:53 +0300

    Initial commit

---
+++ Lesson11/Homework5.1/main.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+#include<set>
+
+int main()
+{
+	
+	
+	std::string new_string;
+	std::cin >> new_string;
+	//std::multiset <char> ms;
+
+	std::vector<char>vector_string;
+
+	for (int i = 0; i < new_string.length(); i++)
+	{
+		char symbol = new_string[i];
+		vector_string.push_back(symbol);
+	}
+	for (int i = 0; i < vector_string.size(); i++)
+	{
+		char index = vector_string[i]
+	}
+	
+	
+	
+
+	return 0;
+}
