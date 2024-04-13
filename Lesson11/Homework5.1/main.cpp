3c2b90e548f427c013e92c0dd51fe97af7e0ecf4
Igor Koshkin <kotoffski82@yandex.ru>
2024-04-13 16:21:24 +0300

    Initial commit

--- Lesson11/Homework5.1/main.cpp
+++ Lesson11/Homework5.1/main.cpp
@@ -1,29 +1,36 @@
 #include<iostream>
 #include<vector>
 #include<set>
+#include<map>
+#include<string>
 
 int main()
 {
-	
-	
 	std::string new_string;
-	std::cin >> new_string;
-	//std::multiset <char> ms;
-
-	std::vector<char>vector_string;
-
+	std::multimap <char, int> map_char;
+	std::getline(std::cin, new_string);
+	int count = 0;
+	
+	for (int i = 0; i < new_string.length(); i++)
+	{	
+		map_char.insert({ new_string[i] , count });
+	}
 	for (int i = 0; i < new_string.length(); i++)
 	{
-		char symbol = new_string[i];
-		vector_string.push_back(symbol);
+		auto key = map_char.find(new_string[i]);
+		if (key == map_char.end())
+			key->second = 1;
+		else
+			key->second++;
 	}
-	for (int i = 0; i < vector_string.size(); i++)
+	
+	for (auto itr = map_char.begin(); itr != map_char.end(); ++itr)
 	{
-		char index = vector_string[i]
+		if (itr->second != 0)
+		{
+			std::cout << itr->first << " : " << itr->second << std::endl;	
+		}
 	}
 	
-	
-	
-
 	return 0;
 }
