a53adc5d0cb0e3415a72de4918296394698c1dae
Igor Koshkin <kotoffski82@yandex.ru>
2024-06-26 17:21:02 +0300

    Add mutex

--- MAP-16/Lesson2(RaceCondMutex)/HomeWork2.2/main.cpp
+++ MAP-16/Lesson2(RaceCondMutex)/HomeWork2.2/main.cpp
@@ -13,39 +13,44 @@
 int num_threads = 5; // Количество потоков
 int calculation_length = 25; // Длина расчета
 std::mutex mtx; // Мутекс для синхронизации вывода
+std::mutex mtx2; // Мутекс для синхронизации остновных функций
 std::vector<char> progress_bar(calculation_length);
 
 void gotoxy()
 {
     
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
     for (int i = 0; i < num_threads; i++)
-    {
+    { 
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::lock_guard<std::mutex> lg2(mtx2);
         COORD coord;
         coord.X = 10;
         coord.Y = i;
         SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
     }
 }
-void progessBar(int id) 
+void progresBar() 
 {
    
     std::clock_t start = std::clock(); // Начало времени выполнения потока
+    std::this_thread::sleep_for(std::chrono::milliseconds(300));
     for (int i = 0; i < calculation_length; ++i)
     {
-       
-        std::this_thread::sleep_for(std::chrono::milliseconds(200));
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::lock_guard<std::mutex> lg(mtx);
             // Отображение прогресс-бара
-            std::lock_guard<std::mutex> lg(mtx);  
-   
-                progress_bar[i] = (char)219;
-                std::cout << progress_bar[i];
+        progress_bar[i] = (char)219;
+        std::cout << progress_bar[i];
+        std::lock_guard<std::mutex> lg2(mtx2);
+                
     }
+    
     // Завершение времени выполнения потока
     std::clock_t end = std::clock();
     double time_spent = static_cast<double>(end - start) / CLOCKS_PER_SEC;
     // Вывод информации о завершении потока
-   std::unique_lock<std::mutex> lg(mtx);
+   std::lock_guard<std::mutex> lg(mtx);
    std::cout <<  std::fixed << std::setprecision(2) << time_spent << "s" << std::endl;
 }
 
@@ -57,14 +62,13 @@ void progessBar(int id)
         //std::cout << "# id    progress bar      time  " << std::endl;
         for (int i = 0; i < num_threads; ++i)
         {
-            
-            std::thread t(progessBar, i);
-            //std::thread g(gotoxy);
-            //g.join();
+            std::thread t(progresBar);
+            std::thread g(gotoxy);
+            g.join();
             std::cout << i << "  " << t.get_id();
             t.join();
     
         } 
-
+ 
         return 0;
     }
